c915f825de2759235e5e180eb195aa96eaea71aa
Tymek Pruszkowski <tymoteuszpruszkowski0@gmail.com>
2026-05-22 22:59:46 +0200

    wire up PC service and apply migrations on startup.

--- APBD-cwiczenia5/Program.cs
+++ APBD-cwiczenia5/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using APBD_Cwiczenia5.Database;
+using APBD_Cwiczenia5.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,11 +8,25 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<IPcService, PcService>();
+
 builder.Services.AddControllers();
+
+builder.Services.AddProblemDetails();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
+app.UseExceptionHandler();
+app.UseStatusCodePages();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
